dc327e1bda2f9bd3bd957487246ee595ef4a4dae
HM-TechDev <tecnologiahmloja@gmail.com>
2025-03-03 16:46:02 -0300

    processar_cards() define como cada card será movido de acordo com o tipo de pedido

--- sync_compras.py
+++ sync_compras.py
@@ -1,49 +1,5 @@
-import requests
 from funcoes_aux import *
-from requisicoes import mover_card
-
-def sincronizar_compras_andamento(pedidos_em_andamento, df_cards, id_fase_pagamento):
-
-    # Percorre a lista contendo o número de todos os pedidos de compra para compará-los com o status no Pipefy
-    ausentes_pipefy = []
-
-    for n_pedido in pedidos_em_andamento:
-        if n_pedido in df_cards['pedido'].values:
-            
-            row = df_cards.loc[df_cards['pedido'] == n_pedido]
-            print(f"Pedido: {row['pedido'].values[0]} ({row['fase_atual'].values[0]})")
-
-            if row['fase_atual'].values[0] == 'Caseado/Botão' or row['fase_atual'].values[0] == 'Embalagem' :
-                print(f"Mover: {row['pedido'].values[0]} ({row['fase_atual'].values[0]})")
-                resultado = mover_card(row, id_fase_pagamento)
-        
-        else:
-            ausentes_pipefy.append(n_pedido)
-    
-    print(f"Pedidos não encontrados no Pipefy {ausentes_pipefy}")
-
-
-def sincronizar_compras_atendidas(pedidos_atendidos, df_cards, id_fase_finalizado):
-
-    # Percorre a lista contendo o número de todos os pedidos de compra para compará-los com o status no Pipefy
-    ausentes_pipefy = []
-
-    # Envia todos os pedidos "Atendidos" no Bling para o status "Finalizado" no Pipefy
-    for n_pedido in pedidos_atendidos:
-        if n_pedido in df_cards['pedido'].values:
-            
-            row = df_cards.loc[df_cards['pedido'] == n_pedido]
-            print(f"Pedido: {row['pedido'].values[0]} ({row['fase_atual'].values[0]})")
-
-            if row['fase_atual'].values[0] == 'Caseado/Botão' or row['fase_atual'].values[0] == 'Embalagem' or row['fase_atual'].values[0] == 'Pagamento':
-                print(f"Mover: {row['pedido'].values[0]} ({row['fase_atual'].values[0]})")
-                resultado = mover_card(row, id_fase_finalizado)
-
-        else:
-            ausentes_pipefy.append(n_pedido)
-    
-    print(f"Pedidos não encontrados no Pipefy {ausentes_pipefy}")
-
+from requisicoes import *
 
 def sincronizar_compras():
 
@@ -61,7 +17,10 @@ def sincronizar_compras():
     id_fase_pagamento = obter_id_fase(pipe_id, 'Pagamento')
     id_fase_finalizado = obter_id_fase(pipe_id, 'Finalizado')
 
-    sincronizar_compras_andamento(pedidos_em_andamento, df_cards, id_fase_pagamento)
-    sincronizar_compras_atendidas(pedidos_atendidos, df_cards, id_fase_finalizado)
+    # Função avalia o tratamento de cada tipo de pedido ao mover os cards. 
+    processar_cards(pedidos_em_andamento, df_cards, id_fase_pagamento, "ANDAMENTO")
+    processar_cards(pedidos_atendidos, df_cards, id_fase_finalizado, "ATENDIDOS")
 
-sincronizar_compras()
+    return "Cards atualizados com sucesso!"
+
+print(sincronizar_compras())
