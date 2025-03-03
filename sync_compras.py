3e626cdcb8a2a02218964bdf657799f7f8b7f1a1
HM-TechDev <tecnologiahmloja@gmail.com>
2025-03-03 15:56:13 -0300

    Elementos comuns usados na movimentação dos cards foram agrupados na função sincronizar_compras()

--- sync_compras.py
+++ sync_compras.py
@@ -2,23 +2,7 @@ import requests
 from funcoes_aux import *
 from requisicoes import mover_card
 
-# Obtém os campos dos cards no pipe
-# Campo desejado: {'id': 'nome_do_solicitante', 'label': 'Pedido'}
-card = obter_campos_pipefy()
-
-def sincronizar_compras_andamento():
-
-    # Lista contendo os múmeros dos pedidos de compra "Em Andamento"
-    pedidos_em_andamento = obter_pedidos_por_status('Em Andamento')
-
-    # Informações (id, nº do pedido e fase atual) sobre os cards que se encontram no pipe 
-    df_cards = obter_cards_pipefy()
-
-    # TESTE - SUBSTITUIR PELO PIPE DESEJADO POSTERIORMENTE 
-    pipe_id = "305715568"
-
-    # Obtém a ID da fase 'Pagamento'
-    id_fase_pagamento = obter_id_fase(pipe_id, 'Pagamento')
+def sincronizar_compras_andamento(pedidos_em_andamento, df_cards, id_fase_pagamento):
 
     # Percorre a lista contendo o número de todos os pedidos de compra para compará-los com o status no Pipefy
     ausentes_pipefy = []
@@ -39,19 +23,7 @@ def sincronizar_compras_andamento():
     print(f"Pedidos não encontrados no Pipefy {ausentes_pipefy}")
 
 
-def sincronizar_compras_atendidas():
-
-    # Lista contendo os múmeros dos pedidos de compra "Atendidos"
-    pedidos_atendidos = obter_pedidos_por_status('Atendido')
-
-    # Obtém as informações (id, nº do pedido e fase atual) sobre os cards que se encontram no pipe 
-    df_cards = obter_cards_pipefy()
-
-    # TESTE - SUBSTITUIR PELO PIPE DESEJADO POSTERIORMENTE 
-    pipe_id = "305715568"
-
-    # Obtém a ID da fase 'Pagamento'
-    id_fase_finalizado = obter_id_fase(pipe_id, 'Finalizado')
+def sincronizar_compras_atendidas(pedidos_atendidos, df_cards, id_fase_finalizado):
 
     # Percorre a lista contendo o número de todos os pedidos de compra para compará-los com o status no Pipefy
     ausentes_pipefy = []
@@ -71,3 +43,25 @@ def sincronizar_compras_atendidas():
             ausentes_pipefy.append(n_pedido)
     
     print(f"Pedidos não encontrados no Pipefy {ausentes_pipefy}")
+
+
+def sincronizar_compras():
+
+    # Lista contendo os múmeros dos pedidos de compra "Em Andamento" e "Atendidos"
+    pedidos_em_andamento = obter_pedidos_por_status('Em Andamento')
+    pedidos_atendidos = obter_pedidos_por_status('Atendido')
+
+    # Informações (id, nº do pedido e fase atual) sobre os cards que se encontram no pipe 
+    df_cards = obter_cards_pipefy()
+
+    # TESTE - SUBSTITUIR PELO PIPE DESEJADO POSTERIORMENTE 
+    pipe_id = "305715568"
+
+    # Obtém a ID das fases 'Pagamento' e 'Finalizado'
+    id_fase_pagamento = obter_id_fase(pipe_id, 'Pagamento')
+    id_fase_finalizado = obter_id_fase(pipe_id, 'Finalizado')
+
+    sincronizar_compras_andamento(pedidos_em_andamento, df_cards, id_fase_pagamento)
+    sincronizar_compras_atendidas(pedidos_atendidos, df_cards, id_fase_finalizado)
+
+sincronizar_compras()
